247bc1661502ecb3b7ef4886681d3e49eb62a298
Mahesh170505 <maheshganesh@umass.edu>
2026-05-29 02:45:45 -0400

    Add duplicate detection method in ContainsDuplicate
    Implement method to check for duplicate integers in an array.

--- Arrays-Hashing/Contains-Duplicate/ContainsDuplicate.java
+++ Arrays-Hashing/Contains-Duplicate/ContainsDuplicate.java
@@ -1,1 +1,14 @@
-
+Class Solution{
+  public static boolean hasDuplicate(int nums[]){
+    Set<Integer> hashSet = new HashSet<>();
+    for(int i = 0; i < nums.length; i++){
+      hashSet.add(nums[i]);
+    }
+    if(hashSet.size() != nums.length){
+      return true;
+    }
+    else{
+      return false;
+    }  
+  }
+}
